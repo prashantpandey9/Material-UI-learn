78a86bfcdc3ec5627787efe9ad42c9621870bf9e
prashantpandey9 <prashantpandey94551@gmail.com>
2020-11-06 17:13:30 +0530

    used save icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button } from '@material-ui/core'
+import SaveIcon  from '@material-ui/icons/Save'
 function App() {
   return (
     <div className="App">
       <header className= "App-header">
-        <Button size='small' variant='contained' color='secondary'>HELLO World!!</Button>
+        <Button 
+          size ='small' 
+          variant ='contained' 
+          color ='secondary'
+          startIcon = {<SaveIcon />}
+        >
+
+          HELLO World!!
+
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
         
       </header>
