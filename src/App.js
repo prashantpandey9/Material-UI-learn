83b44d567d213b605256669a14644dde3507d8c2
prashantpandey9 <prashantpandey94551@gmail.com>
2020-11-06 17:28:50 +0530

    used checkbox and buttonGroup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@material-ui/core'
+import { Button, ButtonGroup, Checkbox } from '@material-ui/core'
 import SaveIcon  from '@material-ui/icons/Save'
+import DeleteIcon  from '@material-ui/icons/Delete'
+
+function CheckboxExample() {
+  const [state, setState] = useState(true)
+  return (
+    <div>
+      <Checkbox 
+        state={state}
+        onChange={(e)=>setState(e.target.state)}
+        inputProps = {{
+          'area-label': 'secondary checkbox'
+        }}
+      />
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className= "App-header">
+      <CheckboxExample />
+      <ButtonGroup variant='contained' color='primary'>
         <Button 
           size ='small' 
           variant ='contained' 
-          color ='secondary'
+          
           startIcon = {<SaveIcon />}
+         
+          size = 'large'
         >
 
-          HELLO World!!
+          Save
 
         </Button>
+        <Button 
+          size ='small' 
+          variant ='contained' 
+          
+          startIcon = {<DeleteIcon />}
+         
+          size = 'large'
+        >
+
+          Discard 
+
+        </Button>
+      </ButtonGroup >
         <img src={logo} className="App-logo" alt="logo" />
         
       </header>
