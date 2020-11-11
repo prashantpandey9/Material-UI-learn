85be74cf335f54e68d8505022fa8135b45cfd37b
prashantpandey9 <prashantpandey94551@gmail.com>
2020-11-11 21:52:42 +0530

    added grid and Paper

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
-import { Button, ButtonGroup, Checkbox, FormControlLabel, TextField } from '@material-ui/core'
+import { Button, ButtonGroup, Checkbox, FormControlLabel, Container } from '@material-ui/core'
 import SaveIcon  from '@material-ui/icons/Save'
 import DeleteIcon  from '@material-ui/icons/Delete'
 import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import { orange } from '@material-ui/core/colors'
 import 'fontsource-roboto';
 import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+
 const useStyles = makeStyles({
   root: {
     background: 'linear-gradient(45deg, #FE688B, #FF8E53)',
@@ -70,52 +73,67 @@ function CheckboxExample() {
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className= "App-header">
-          <Typography variant='h2'>
-           Heading 2
-          </Typography>
-          <Typography variant='subtitle1'>
-            Subtitle1
-          </Typography>
-          <Typography variant='body1'>
-            Body 1
-          </Typography>
-          <ButtonStyled />
-          <TextField 
-            variant='outlined'
-            color='secondary'
-            type='email'
-            label='The Time'
-            placeholder='email.com'
-          />
-          <CheckboxExample />
-          <ButtonGroup variant='contained' color='primary'>
-            <Button 
-              variant ='contained' 
+      <Container maxWidth='xs'>
+        <div className="App">
+          <header className= "App-header">
+            <Typography variant='h2'>
+            Heading 2
+            </Typography>
+            <Typography variant='subtitle1'>
+              Subtitle1
+            </Typography>
+            <Typography variant='body1'>
+              Body 1
+            </Typography>
+            <ButtonStyled />
+
+            <Grid container spacing={2} justify="center">
+              <Grid item>
+                <Paper style={{height: 75, width: 50,}} />
+              </Grid>
+              <Grid item>
+                <Paper style={{height: 75, width: 50,}} />
+              </Grid>
+              <Grid item>
+                <Paper style={{height: 75, width: 50,}} />
+              </Grid>
               
-              startIcon = {<SaveIcon />}
+            </Grid>
+            {/* <TextField 
+              variant='outlined'
+              color='secondary'
+              type='email'
+              label='The Time'
+              placeholder='email.com'
+            /> */}
+            <CheckboxExample />
+            <ButtonGroup variant='contained' color='primary'>
+              <Button 
+                variant ='contained' 
+                
+                startIcon = {<SaveIcon />}
+              
+                size = 'large'
+              >
+
+                Save
+
+              </Button>
+              <Button 
+                size ='large' 
+                variant ='contained' 
+                startIcon = {<DeleteIcon />}
+              >
+
+                Discard 
+
+              </Button>
+            </ButtonGroup >
+              <img src={logo} className="App-logo" alt="logo" />
             
-              size = 'large'
-            >
-
-              Save
-
-            </Button>
-            <Button 
-              size ='large' 
-              variant ='contained' 
-              startIcon = {<DeleteIcon />}
-            >
-
-              Discard 
-
-            </Button>
-          </ButtonGroup >
-            <img src={logo} className="App-logo" alt="logo" />
-          
-        </header>
-      </div>
+          </header>
+        </div>
+      </Container>
     </ThemeProvider> 
   );
 }
